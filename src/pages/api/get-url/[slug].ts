e604b7f342d3e0bb30e1e82454c7a7cd933b47ee
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-06-16 12:11:51 +0100

    ad get-url path

---
+++ src/pages/api/get-url/[slug].ts
@@ -0,0 +1,24 @@
+import { prisma } from '../../../db/client';
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const slug = req.query["slug"]
+    if (!slug || typeof slug !== "string") 
+        return res.status(404).send(JSON.stringify({ message: "Please you need to pass a slug" }))
+        
+    const data = await prisma.shortLink.findFirst({
+        where: {
+            slug: {
+                equals: slug
+            }
+        }
+    })
+    if (!data) return res.status(404).send(JSON.stringify({ message: "Invalid slug" }))
+    
+    // return res.redirect(data.url)
+    return res.json(data)
+    
+   
+}
